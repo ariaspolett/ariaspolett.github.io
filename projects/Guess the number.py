c8b8290f22cf4c1127d4640f114a13e288525b31
ariaspolett <ariaspolett21@gmail.com>
2025-05-06 12:57:55 -0700

    took out two projects

--- projects/Guess the number.py
+++ projects/Guess the number.py
@@ -1,22 +1,21 @@
-#Generate a random number between 1 and 20 (including both). Ask the user to guess the number and let them know if their guess is too low, too high, or exactly right. 🎲
-#Extras:
-#Keep the game going until the user types “exit” 🔄
-#Track how many guesses the user has made and show this count when the game ends 📊
-#Start the user at 20 points and take one away for each guess.
-#Remember the user's all time high score.
-#Use Tkinter to create a GUI game interface
-
 import random
-mylist = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
+mylist = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50]
 number = random.choice(mylist)
 
-points = 20 
+points = 10 
 guesses_made = 0
 guess = 0
-print("You have 20 points, guess correctly before you run out of points. 3417 stands for exit, enter that if you want to end the game.")
-while points > 0 and guess != 3417 and guess != number:
-    guess = int(input("Guess a number between 1-20."))
-    if guess > number:
+print("You have 10 points, guess correctly before you run out of points. 3817 stands for exit, enter that if you want to end the game.")
+while points > 0 and guess != 3817 and guess != number:
+    guess = int(input("Guess a number between 1-50: "))
+    if guess > 50 and guess != 3817:
+        print("Please guess a number within 1-50.")
+        continue
+
+    if guess == 3817:
+        print("Thank you for playing! Play again")
+        break
+    elif guess > number:
         guesses_made += 1
         points -= 1
         print("Guess was too high.")
@@ -28,10 +27,7 @@ while points > 0 and guess != 3417 and guess != number:
         guesses_made += 1
         points -= 1
         print("Exactly right!")
-    elif guess == 3417:
-        break
+        print("You had " + str(points) + " points left!")
+        print("You found the number after " + str(guesses_made) + " guesses!")
     else:
         print("invalid input")
-
-print("You had " + str(points) + " points left!")
-print("You found the number after " + str(guesses_made) + " guesses!")
---
+++ projects/game.py
@@ -0,0 +1,152 @@
+import random
+from colorama import Fore, Style, init
+
+init(autoreset=True)
+
+# Word themes (same as before)
+themes = {
+    "animals": ["aardvark", "alligator", "ant", "anteater", "antelope", "ape", "baboon", "badger", "bat", "bear", "beaver", "bee", "bison", "boar", "buffalo", "butterfly", "camel", "capybara", "caribou", "cat", "caterpillar", "cattle", "cheetah", "chicken", "chimpanzee", "clam", "cobra", "cockroach", "coyote", "crab", "crane", "crocodile", "crow", "deer", "dinosaur", "dog", "dolphin", "donkey", "dormouse", "dove", "dragonfly", "duck", "eagle", "echidna", "eel", "elephant", "elk", "emu", "falcon", "ferret", "finch", "fish", "flamingo", "fly", "fox", "frog", "gazelle", "gerbil", "giraffe", "goat", "goldfinch", "goldfish", "goose", "gorilla", "grasshopper", "gull", "hamster", "hare", "hawk", "hedgehog", "heron", "hippopotamus", "hornet", "horse", "human", "hummingbird", "hyena", "jackal", "jaguar", "jay", "jellyfish", "kangaroo", "koala", "lemur", "leopard", "lion", "llama", "lobster", "locust", "manatee", "mantis", "meerkat", "mole", "mongoose", "monkey", "moose", "mosquito", "mouse", "mule", "narwhal", "newt", "nightingale", "octopus", "opossum", "ostrich", "otter", "owl", "ox", "oyster", "panda", "panther", "parrot", "pelican", "penguin", "pig", "pigeon", "polar-bear", "pony", "porcupine", "porpoise", "quail", "quetzal", "rabbit", "raccoon", "ram", "rat", "raven", "red-panda", "reindeer", "rhinoceros", "salamander", "salmon", "sardine", "scorpion", "seahorse", "seal", "shark", "sheep", "skunk", "snail", "snake", "sparrow", "spider", "squid", "squirrel", "starling", "stingray", "stoat", "stork", "swan", "termite", "tiger", "toad", "turkey", "turtle", "viper", "vulture", "wallaby", "walrus", "wasp", "weasel", "whale", "wildcat", "wolf", "wolverine", "woodpecker", "worm", "yak", "zebra"],
+    "food": ["apple", "banana", "pear", "mango", "orange", "carrot", "broccoli", "avocado", "tomato", "onion", "sourcream", "butter", "eggs", "yogurt", "milk", "cheese", "pizza", "sushi", "pancakes", "frenchtoast", "pasta", "burger", "salad", "fries", "cake", "cookie", "pie", "cupcake"],
+    "ice_cream": ["vanilla", "chocolate", "strawberry", "blueberry", "cottoncandy", "mintchocolatechip", "mint", "cookiedough", "rockyroad", "cookiesandcream", "pistachio", "butterpecan"],
+    "instruments": ["guitar", "piano", "drums", "violin", "viola", "triangle", "maracas", "clarinet", "flute", "trumpet", "saxophone", "cello", "harp", "bass"],
+}
+
+art = {
+    0: (
+        "  +---+",
+        "  |   |",
+        "      |",
+        "      |",
+        "      |",
+        "      |",
+        "========"
+    ),
+    1: (
+        "  +---+",
+        "  |   |",
+        "  O   |",
+        "      |",
+        "      |",
+        "      |",
+        "========"
+    ),
+    2: (
+        "  +---+",
+        "  |   |",
+        "  O   |",
+        "  |   |",
+        "      |",
+        "      |",
+        "========"
+    ),
+    3: (
+        "  +---+",
+        "  |   |",
+        "  O   |",
+        " /|   |",
+        "      |",
+        "      |",
+        "========"
+    ),
+    4: (
+        "  +---+",
+        "  |   |",
+        "  O   |",
+        " /|\\  |",
+        "      |",
+        "      |",
+        "========"
+    ),
+    5: (
+        "  +---+",
+        "  |   |",
+        "  O   |",
+        " /|\\  |",
+        " /    |",
+        "      |",
+        "========"
+    ),
+    6: (
+        "  +---+",
+        "  |   |",
+        "  O   |",
+        " /|\\  |",
+        " / \\  |",
+        "      |",
+        "========"
+    )
+}
+
+def display_man(wrong_guesses):
+    print(Fore.CYAN + "***********")
+    for line in art[wrong_guesses]:
+        print(Fore.RED + line + Style.RESET_ALL)
+    print(Fore.CYAN + "***********")
+
+def display_hint(hint):
+    print(" ".join(hint))
+
+def display_answer(answer):
+    print("Answer was:", Fore.GREEN + " ".join(answer))
+
+def choose_theme():
+    print("Choose a word theme:")
+    for i, theme in enumerate(themes.keys(), 1):
+        print(f"{i}. {theme.capitalize()}")
+    choice = int(input("Enter the number of your choice: "))
+    theme_name = list(themes.keys())[choice - 1]
+    return theme_name
+
+# Main game function
+def main():
+    print("Hangman Game")
+    while True:  # Loop to allow restarting the game
+        theme_name = choose_theme()  # Player selects a theme
+        words = themes[theme_name]  # Get the word list based on the selected theme
+
+        answer = random.choice(words)
+        hint = ["_"] * len(answer)
+        wrong_guesses = 0
+        guessed_letters = set()
+        is_running = True
+
+        while is_running:
+            display_man(wrong_guesses)
+            display_hint(hint)
+            guess = input("Enter a letter: ").lower()
+
+            if len(guess) != 1 or not guess.isalpha():
+                print(Fore.YELLOW + "Invalid input. Please try again.")
+                continue
+
+            if guess in guessed_letters:
+                print(Fore.YELLOW + f"{guess} has already been guessed. Enter a different letter.")
+                continue
+
+            guessed_letters.add(guess)
+
+            if guess in answer:
+                for i in range(len(answer)):
+                    if answer[i] == guess:
+                        hint[i] = guess
+            else:
+                wrong_guesses += 1
+
+            if "_" not in hint:
+                display_man(wrong_guesses)
+                display_answer(answer)
+                print(Fore.GREEN + "🎉 You won! 🎉")
+                is_running = False
+            elif wrong_guesses >= len(art) - 1:
+                display_man(wrong_guesses)
+                display_answer(answer)
+                print(Fore.RED + "💀 You lost! 💀")
+                is_running = False
+        
+        play_again = input("Want to play again? (y/n): ").lower()
+        if play_again != "y":
+            print("Thank you for playing! Play again soon!")
+            break  # Exit the game loop if they don't want to play again
+
+if __name__ == "__main__":
+    main()
